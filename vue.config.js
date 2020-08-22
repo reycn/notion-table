4ebcc5da7c4874da9d74355b2f189cab81294208
Reynard <rey@pku.edu.cn>
2020-08-22 14:00:16 +0800

    Deployment

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,13 @@
+let BASE_URL = '/'
+switch (process.env.DEPLOY) {
+   case 'online':
+      BASE_URL = 'https://cdn.jsdelivr.net/gh/reycn/notion-table@gh-pages/'
+      break
+   default:
+      BASE_URL = '/'
+}
 module.exports = {
+   publicPath: BASE_URL,
    configureWebpack: {
       // plugins: [new webpack.IgnorePlugin(/^\.\/locale$/, /moment$/)],
       // CDN 加载组件
